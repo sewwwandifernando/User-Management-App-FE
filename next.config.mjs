cd955c7c5190776d85c05ae20bda161f4f746cdc
sewwwandifernando <sewwandinirushika7@gmail.com>
2025-09-14 01:33:09 +0530

    Add initial project setup with README, configuration files, and global styles

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+// next.config.mjs
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
